41d05627d47e2034ed93f6b07d0f0c144d7b52d3
wenqinchao <18782417423@163.com>
2021-03-20 22:07:46 +0800

    second

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-  long_description = fh.read()
+    long_description = fh.read()
 
 setuptools.setup(
-  name="src",
-  version="0.0.1",
-  author="Wen",
-  author_email="wenqinchao@gmail.com",
-  description="A package interact with bitcoin node",
-  long_description=long_description,
-  license="MIT",
-  url="https://github.com/wenqinchao/btc",
-  packages=setuptools.find_packages(),
-  classifiers=[
-  "Programming Language :: Python :: 3",
-  "License :: OSI Approved :: MIT License",
-  "Operating System :: OS Independent",
-  ],
+    name="btc",
+    version="0.0.1",
+    author="Wen",
+    author_email="wenqinchao@gmail.com",
+    description="A package interact with bitcoin node",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/wenqinchao/btc",
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6"
 )
