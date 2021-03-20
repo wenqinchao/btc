e0bdc388a98209cc90ced77694d138b9900550e1
wenqinchao <18782417423@163.com>
2021-03-20 22:30:20 +0800

    f

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="btc_cj",
-    version="0.0.1",
+    version="1.0.0",
     author="Wen",
     author_email="wenqinchao@gmail.com",
     description="A package interact with bitcoin node",
