0a9c2293c1527d80cef31eb6632e453730df8a6a
wenqinchao <18782417423@163.com>
2021-03-20 22:21:20 +0800

    s

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="btc",
+    name="btc_cj",
     version="0.0.1",
     author="Wen",
     author_email="wenqinchao@gmail.com",
